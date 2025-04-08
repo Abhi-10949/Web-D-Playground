96dfb687139ef2eb7c08d7ad2bbbd94cca5800d9
Abhi-10949 <23bcs10949@cuchd.in>
2025-04-08 13:43:53 +0530

    added JS

---
+++ MERN_STACK/Java Script/app.js
@@ -0,0 +1,35 @@
+console.log("Hello World!");
+console.log("Abhishek Kumar");
+
+let a = 10;
+let b = 5;
+console.log("Sum is : ", a+b);
+
+let pencil = 10;
+let eraser = 5;
+// let output = "The total price : "+ (pencil + eraser)+ " Rupees";
+
+// This is template literals
+let output = `The total price : ${pencil+eraser} Rupees.`;  //this is called back ticks
+console.log(output);
+
+// arithmetic operator 
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+console.log(a%b);
+
+// == this operator only compares the values not the type of the value
+// === this operator compares the values and also the type of the value
+let n = 5; 
+let str = '5';
+console.log(n == str); // == is used and it will give true
+console.log(n === str); // === is used and it will give false
+
+// if condition 
+let firstName = "Abhishek";
+if (firstName == "Abhishek"){
+    console.log(`Welocome ${firstName}`);
+}
