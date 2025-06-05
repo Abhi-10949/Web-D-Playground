a7be75af583fe577fc86321c2da27100aeb79b33
Abhi-10949 <23bcs10949@cuchd.in>
2025-06-05 19:51:58 +0530

    Modified JS

---
+++ MERN_STACK/Java Script/app.js
@@ -0,0 +1,4 @@
+console.dir(document.querySelectorAll("h1"));
+console.dir(document.querySelector(".oldImg"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelectorAll("div a"))
