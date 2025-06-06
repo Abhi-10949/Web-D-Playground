975015688308eb0a43ac3f748c49fba0befa6860
Abhi-10949 <23bcs10949@cuchd.in>
2025-06-06 20:31:12 +0530

    modified and added

--- MERN_STACK/Java Script/app.js
+++ MERN_STACK/Java Script/app.js
@@ -1,4 +1,18 @@
-console.dir(document.querySelectorAll("h1"));
-console.dir(document.querySelector(".oldImg"));
-console.dir(document.querySelector("#description"));
-console.dir(document.querySelectorAll("div a"))
+// console.dir(document.querySelectorAll("h1"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelectorAll("div a"))
+
+let links = document.querySelectorAll('.box a');
+
+
+// this is with the help of for loop
+// for(let i = 0 ;i<links.length;i++){
+//   links[i].style.color = "yellow";
+// }
+
+
+// this is with the help of for-of loop
+for(link of links){
+  link.style.color = "yellow";
+}
