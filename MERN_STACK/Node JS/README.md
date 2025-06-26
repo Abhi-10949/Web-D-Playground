db150465d3678cce73c8a1bf1add05781b3e86c8
Abhi-10949 <23bcs10949@cuchd.in>
2025-06-26 20:01:59 +0530

    modified README

--- MERN_STACK/Node JS/README.md
+++ MERN_STACK/Node JS/README.md
@@ -8,3 +8,9 @@
 ## Node REPL (Read, Evaluate, Print and Loop)
 * .help :- gives us commands
 * Node Files:- node fileName.js
+* Process: This object provides  information about, and control over, the current Node.js process.
+* process.argv:- returns an array containing the command-line arguments passed when the Node.js process was launched.
+
+# module.exports (it is a special object)
+* it is requiring files
+* require() :- a built-in function to include external modules that exist in separate files.
--- MERN_STACK/Node JS/script.js
+++ MERN_STACK/Node JS/script.js
@@ -1,11 +1,18 @@
-let n = 5;
-for(let i = 0;i<n;i++){
-    console.log("Hello",i);
-}
+// let n = 5;
+// for(let i = 0;i<n;i++){
+//     console.log("Hello",i);
+// }
 
-console.log("Bye!");
+// console.log("Bye!");
 
-let n1=[1,2,3,4,5];
-for(i of n1){
-    console.log("index's", i);
+// let n1=[1,2,3,4,5];
+// for(i of n1){
+//     console.log("index's", i);
+// }
+
+// console.log(process.argv);
+
+let args = process.argv;
+for(let i = 2;i<args.length;i++){
+    console.log("Hello to ", args[i]);
 }
