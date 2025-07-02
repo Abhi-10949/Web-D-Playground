53bc13ab3444dbb537260b1e766c8dc3f5fe52a2
Abhi-10949 <23bcs10949@cuchd.in>
2025-07-02 20:23:37 +0530

    working with the express dir

--- MERN_STACK/ExpressDir/index.js
+++ MERN_STACK/ExpressDir/index.js
@@ -14,6 +14,42 @@ app.listen(port, ()=>{
 // ctrl c and on the server side type localhost:3000, ensure that the server
 // is get started
 
-app.use((req, res)=>{
-    console.log("request recevied");
-});
+// app.use((req, res)=>{
+//     // console.log(req);
+//     console.log("request recevied");
+//     // res.send("this is the basic response");
+//     let code  = "<h1>Fruits</h1> <ul><li>apple</li><orange</li></ul>";
+//     res.send(code);
+// });
+
+
+// NOTE: res.send() this function is used to send the respond to the browser with
+// the help of server
+// We can able to send only one responce for the same path it may be either : snigle string
+// single HTML page  or it may be a single object
+
+
+
+app.get("/", (req, res)=>{
+    res.send("you contacted to root path");
+})
+
+app.get("/search", (req, res)=> {
+    res.send("you contacted to search path");
+})
+
+app.get("/about", (req, res)=>{
+    res.send("you contected to about path");
+})
+
+
+// We can treat this route as the while case 😉
+// For any GET request that doesn’t match any 
+// previously defined route, send this response.
+// app.get("*", (req, res)=>{
+//     res.send("this path does not exist.");
+// })
+
+app.post("/", (req,res)=>{
+    res.send("you sent a post req to root");
+})
