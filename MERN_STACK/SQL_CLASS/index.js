0811c7b8ab6083d214a06234eb433dd23c16aa32
Abhi-10949 <23bcs10949@cuchd.in>
2025-08-01 15:42:02 +0530

    Add SQL_CLASS folder with initial files

---
+++ MERN_STACK/SQL_CLASS/index.js
@@ -0,0 +1,21 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD
+});
+
+let getRandomUser = ()=>{
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(), // before version 9.1.0, use userName()
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
+
+console.log(getRandomUser());
