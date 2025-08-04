4eb0b7cf75b0decd9c97ff9f795df525b180d4ab
Abhi-10949 <23bcs10949@cuchd.in>
2025-08-04 14:58:28 +0530

    working with ejs and sql

--- MERN_STACK/SQL_CLASS/index.js
+++ MERN_STACK/SQL_CLASS/index.js
@@ -1,6 +1,12 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 require('dotenv').config();
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("veiw engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -23,29 +29,29 @@ let getRandomUser = ()=>{
 // let query = "SHOW TABLES";
 
 // inserting new data usig placeholder. (mannually)
-let query = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let query = "INSERT INTO user (id, username, email, password) VALUES ?";
 // let user = [["123b", "123_newuserb", "abc@gmail.comb", "abcb"],
 //             ["123c", "123_newuserc", "abc@gmail.comc", "abcc"]];
 
-let data =[];
-for(let i =1; i<=100;i++){
-    data.push(getRandomUser()); // 100 fake user's data
-}
+// let data =[];
+// for(let i =1; i<=100;i++){
+//     data.push(getRandomUser()); // 100 fake user's data
+// }
 
 
-try{
-    connection.query(query, [data], (err, results)=>{
-    if(err) throw err;
-    console.log(results); // results contains rows returned by server
-    // console.log(results.length);
-    // console.log(results[0]);
-    // console.log(results[1]);
-})
-} catch(err){
-    console.log(err);
-}
+// try{
+//     connection.query(query, [data], (err, results)=>{
+//     if(err) throw err;
+//     console.log(results); // results contains rows returned by server
+//     // console.log(results.length);
+//     // console.log(results[0]);
+//     // console.log(results[1]);
+// })
+// } catch(err){
+//     console.log(err);
+// }
 
-connection.end();
+// connection.end();
 
 // let getRandomUser = ()=>{
 //   return {
@@ -57,3 +63,43 @@ connection.end();
 // }
 
 // console.log(getRandomUser());
+
+// Home page
+app.get ("/", (req, res)=>{
+    let q = `SELECT COUNT(*) FROM user`;
+    try{
+    connection.query(q, (err, results)=>{
+    if(err) throw err;
+    let count = results[0]["COUNT(*)"];
+    // console.log(results[0]["COUNT(*)"]); // results contains rows returned by server
+    res.render("home.ejs", {count});
+    })
+    } catch(err){
+        console.log(err);
+        res.render("some error in database!");
+    }
+});
+
+// Show Route
+app.get("/user", (req, res)=>{
+    let q = `SELECT * FROM user`;
+
+    try{
+    connection.query(q, (err, users)=>{
+    if(err) throw err;
+    // console.log(results);
+    // res.send(results);
+    res.render("showuser.ejs", {users});
+    })
+    } catch(err){
+        console.log(err);
+        res.render("some error in database!");
+    }
+});
+
+// Edit Route
+
+
+app.listen("8080", ()=>{
+    console.log("Serever is listning to port 8080...");
+});
