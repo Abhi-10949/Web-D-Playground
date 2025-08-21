985af28e6c530e31abbd65d4180f6b39dac710a9
Abhi-10949 <23bcs10949@cuchd.in>
2025-08-21 10:45:52 +0530

    Competed with Mongo3

--- MERN_STACK/MONGO3/index.js
+++ MERN_STACK/MONGO3/index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true})); // this is used to parse the data
+app.use(methodOverride("_method")); // this is used to override the method of the form submission
 
 main().then(()=>{console.log("connection successful")})
 .catch(err => console.log(err));
@@ -53,11 +55,23 @@ app.get("/chats/:id/edit", async (req, res)=>{
 });
 
 // Update Route
-app.put("chats/:id", async (req, res)=>{
+app.put("/chats/:id", async (req, res)=>{
     let {id} = req.params;
-    let {newmsg} = req.body; 
+    let {msg: newmsg} = req.body;
+    // console.log(newmsg); 
     let updatedChat = await Chat.findByIdAndUpdate(id, {msg: newmsg}, 
         {runValidators: true}, {new: true});
+
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+//Destroy Route
+app.delete("/chats/:id", async (req, res)=>{
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
 })
 
 // let chat1 = new Chat({
